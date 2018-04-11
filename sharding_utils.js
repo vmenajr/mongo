8cbefa887f7c7653b5ff4ec295468ffb45811644
Vick Mena <vickmena@gmail.com>
2018-04-11 13:24:28 -0400

    Sharding changelog utils

---
+++ sharding_utils.js
@@ -0,0 +1,180 @@
+function sendtoscreen(obj) {
+	printjson(obj.toArray())
+}
+
+function configDB() {
+	return db.getSiblingDB("config");
+}
+
+oldhelp=sh.help
+sh.help = function() {
+	oldhelp()
+	print("\tsh.op_count()")
+	print("\tsh.ops_by_hour()")
+	print("\tsh.ops_by_hour_not_aborted()")
+	print("\tsh.ops_by_hour_not_aborted_condensed()")
+	print("\tsh.ops_by_ns()")
+	print("\tsh.splits_and_migrations()")
+	print("\tsh.errors_per_phase()")
+	print("\tsh.covered_period()")
+	print("\tsh.first_last_migration()")
+	print("\tsh.shards_by_donor()")
+	print("\tsh.rates_and_volumes()")
+}
+
+sh.op_count = function() {
+	sendtoscreen(
+		configDB().changelog.aggregate([
+			{ $group : { _id : { what : "$what", note : "$details.note" }, total : { $sum : 1  } } } 
+		])
+	)
+}
+
+sh.ops_by_hour = function() {
+	sendtoscreen(
+		configDB().changelog.aggregate([
+			{ $project : { day : { $dayOfYear : "$time" }, time : { $hour : "$time" }, what : "$what", note : "$details.note" } }, 
+			{ $group : { _id : { day : "$day", time : "$time", what : "$what", note : "$note" }, count : { $sum : 1 } } }, 
+			{ $sort : { "_id.day" : 1, "_id.time" : 1 } } 
+		])
+	)
+}
+
+sh.ops_by_hour_not_aborted = function() {
+	sendtoscreen(
+		configDB().changelog.aggregate([
+			{ $match : { "details.note" : { $ne : 'aborted' } } },
+			{ $project : { day : { $dayOfYear : "$time" }, time : { $hour : "$time" }, what : "$what" } },
+			{ $group : { _id : { day : "$day", time : "$time", what : "$what" }, count : { $sum : 1 } } },
+			{ $sort : { "_id.day" : 1, "_id.time" : 1 } }
+		])
+	)
+}
+
+sh.ops_by_hour_not_aborted_condensed = function() {
+	configDB().changelog.aggregate([
+		{ $match : { "details.note" : { $ne : 'aborted' } } },
+		{ $project : { day : { $dayOfYear : "$time" }, time : { $hour : "$time" }, what : "$what" } },
+		{ $group : { _id : { day : "$day", time : "$time", what : "$what" }, count : { $sum : 1 } } },
+		{ $sort : { "_id.day" : 1, "_id.time" : 1 } }
+	]).forEach(function(cl){ printjsononeline(cl);});
+}
+
+sh.ops_by_ns = function() {
+	sendtoscreen(
+		configDB().changelog.aggregate([
+			{ $group : { _id : { what : "$what", ns : "$ns", note : "$details.note" }, total : { $sum : 1  } } },
+			{ $sort : { "_id.ns" : 1, "_id.what" : 1 } } 
+		])
+	)
+}
+
+sh.splits_and_migrations = function() {
+	sendtoscreen(
+		configDB().changelog.aggregate([
+			{$group: {
+				_id:{ "ns":"$ns","server":"$server"},
+				multiSplits:{$sum:{$cond:[{$eq:["$what","multi-split"]},1,0]}},
+				splits:{$sum:{$cond:[{$eq:["$what","split"]},1,0]}},
+				migrationAttempts:{$sum:{$cond:[{$eq:["$what","moveChunk.from"]},1,0]}},
+				migrationFailures:{$sum:{$cond:[ {$eq:["$details.note","aborted" ]} ,1,0]}},
+				migrations:{$sum:{$cond:[{$eq:["$what","moveChunk.commit"]},1,0]}}
+			} },
+			{ $sort: { _id:1, multiSplits: -1, splits: -1 } }
+		])
+	)
+}
+
+sh.errors_per_phase = function() {
+	sendtoscreen(
+		configDB().changelog.aggregate([
+			{ $match : { "details.note" : 'aborted' } },
+			{ $group : { _id : { what : "$what", errmsg : "$details.errmsg" }, count : { $sum : 1 } } },
+			{ $sort : { "_id.what" : 1, count : -1 } }
+		])
+	)
+}
+
+sh.covered_period = function() {
+	sendtoscreen( configDB().changelog.find({},{_id:0, time:1}).limit(1) )
+	sendtoscreen( configDB().changelog.find({},{_id:0, time:1}).sort({$natural:-1}).limit(1) )
+}
+
+sh.first_last_migration = function() {
+	sendtoscreen( configDB().changelog.find({what:"moveChunk.commit"},{_id:0, time:1}).limit(1) )
+	sendtoscreen( configDB().changelog.find({what:"moveChunk.commit"},{_id:0, time:1}).sort({$natural:-1}).limit(1) )
+}
+
+sh.shards_by_donor = function() {
+	sendtoscreen(
+		configDB().changelog.aggregate([
+			{ $match: { "what" : "moveChunk.start" }},
+			{ $group : { _id: { from: "$details.from", ns : "$ns"}, count: { $sum : 1 } } },
+			{ $sort : { "count" : -1 } }
+		])
+	)
+}
+
+sh.rates_and_volumes = function() {
+	sendtoscreen(
+		configDB().changelog.aggregate([
+			{ $match: { what: { "$in": [ "moveChunk.commit", "moveChunk.start" ] } } },
+			{ $project: { _id: 0,
+				what: "$what", time: "$time",
+				uniqueDetails: {
+					from: "$details.from", to: "$details.to",
+					mink: "$details.min", maxk: "$details.max",
+					ns: "$ns" },
+				sizeInfo: {
+					cloned: "$details.cloned",
+					bytes: "$details.clonedBytes" }, } },
+			{ $group: {
+				_id: "$uniqueDetails",
+				start: { "$min": "$time" },
+				commit: { "$max": "$time" },
+				count: { "$sum": 1 },
+				cloned: { "$max": "$sizeInfo.cloned"},
+				bytes: { "$max": "$sizeInfo.bytes" } } },
+			{ $project: { _id: "$_id",
+				whenStart: "$start", whenDone: "$commit",
+				bytesMoved: "$bytes", docsMoved: "$cloned",
+				moveTime_ms: { "$subtract": [ "$commit", "$start" ] } } },
+			{ $match: { bytesMoved: { "$ne": null }, moveTime_ms: { "$ne": 0 } } },
+			{ $project: { _id: "$_id",
+				whenStart: "$whenStart", whenDone: "$whenDone",
+				moveTime_ms: "$moveTime_ms",
+				bytesMoved: "$bytesMoved", docsMoved: "$docsMoved",
+				bytesPer_ms: { "$divide": [ "$bytesMoved", "$moveTime_ms" ] },
+				docsPer_ms: { "$divide": [ "$docsMoved", "$moveTime_ms" ] } } },
+			// outputs stats for each chunk moved ...
+			{ $project: { _id: "$_id",
+				whenStart: "$whenStart", whenDone: "$whenDone",
+				moveTime_ms: "$moveTime_ms",
+				bytesMoved: "$bytesMoved", docsMoved: "$docsMoved",
+				docsPer_sec: { "$multiply": [ "$docsPer_ms", 1000.0 ] },
+				MBper_sec: { "$divide": [ "$bytesPer_ms", 1048.576 ] } } }
+			// optionally limit to date range, or etc...
+			// , { $match: {
+			//    whenStart: { "$gte": ISODate("2017-08-09T00:00:00.000Z") },
+			//    whenDone: { "$lt": ISODate("2017-08-10T00:00:00.000Z") } } }
+			// optionally get averages per shard-pair, or per sending shard or receiving shard or per collection, etc.
+			// , { $group: {
+			//    _id: "$_id.from",  // example: from-shard stats
+			//    // _id: { "$_id.from", "$_id.to" }, // example: shard pair stats,
+			//    // _id: { "$_id.ns" }, // example: collection chunk stats
+			//    minMoveTime_ms: { "$min": "$moveTime_ms" },
+			//    avgMoveTime_ms: { "$avg": "$moveTime_ms" },
+			//    maxMoveTime_ms: { "$max": "$moveTime_ms" },
+			//    stdevMoveTime_ms: { "$stdDevPop": "$moveTime_ms" },
+			//    minMBper_sec: { "$min": "$MBper_sec" },
+			//    avgMBper_sec: { "$avg": "$MBper_sec" },
+			//    maxMBper_sec: { "$max": "$MBper_sec" },
+			//    stdevMBper_sec: { "$stdDevPop": "$MBper_sec" } } }
+			// optionally, sort for time charting or hot shards by long-time or large volume, etc., output to collection, etc.
+			//, { $sort: { whenDone: 1 } }
+			//, { $out: "__chunkMoveStats__" }
+		])
+	)
+}
+
+
