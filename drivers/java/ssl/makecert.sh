b5e106e1421418633efbb32876cf0a44da3046fb
Vick Mena <vickmena@gmail.com>
2018-05-18 10:33:37 -0400

    Simply and allow the creation of multiple certs with different host names

--- drivers/java/ssl/makecert.sh
+++ drivers/java/ssl/makecert.sh
@@ -6,7 +6,7 @@ state="SomeState"
 city="SomeCity"
 org="SomeOrganization"
 unit="SomeOrganizationalUnit"
-host=$(hostname -f)
+hosts=$(hostname -f)
 email="root@${host}"
 
 abend() {
@@ -16,7 +16,7 @@ abend() {
 }
 
 usage() { 
-    echo "Usage: $0 [-d days] [-s state] [-c city] [-o org] [-u unit] [-h host] [-e email]"
+    echo "Usage: $0 [-d days] [-s state] [-c city] [-o org] [-u unit] [-h hosts] [-e email]"
     echo "  bits:    Number of bits ($bits)"
     echo "  days:    Number of days ($days)"
     echo "  country: Country code ($country)"
@@ -24,7 +24,8 @@ usage() {
     echo "  city:    City ($city)"
     echo "  org:     Organization ($org)"
     echo "  unit:    Organizational unit ($unit)"
-    echo "  host:    Fully qualified domain name for the server using this cert ($host)"
+    echo "  hosts:   Fully qualified domain name for the server(s) using this cert ($host)."
+    echo "           For multiple certs surround with quotes (e.g. -h \"host1 host2 ...\")"
     echo "  email:   Email associated with the cert ($email)"
     echo
     exit 1
@@ -54,7 +55,7 @@ while getopts ":b:d:k:s:c:o:u:h:e:" o; do
             unit=${OPTARG}
             ;;
         h)
-            host=${OPTARG}
+            hosts=${OPTARG}
             ;;
         e)
             email=${OPTARG}
@@ -66,64 +67,42 @@ while getopts ":b:d:k:s:c:o:u:h:e:" o; do
 done
 shift $((OPTIND-1))
 
-answers() {
-	echo $country
-	echo $state
-	echo $city
-	echo $org
-	echo $unit
-	echo $host
-	echo $email
-}
 subject_template="/C=${country}/ST=${state}/L=${city}/O=${org}/OU=${unit}/CN="
 
 [[ "Darwin" -eq "$(uname -s)" ]]  && configFile=/System/Library/OpenSSL/openssl.cnf || configFile=/etc/ssl/openssl.cnf
 baseConfig=$(grep -v ^# ${configFile} | tr -s \\n)
+
 ext="
 [ EXT ]
-basicConstraints=CA:FALSE
 extendedKeyUsage=serverAuth,clientAuth
 "
+
 config="
 $baseConfig
 $ext
 "
 
-rm *.csr *.crt *.key *.pem
-#set -x
 # Root Certificate Authority and private key
-#openssl genrsa -out root-ca.key ${bits}
 openssl req -x509 -new -newkey rsa:${bits} -nodes -keyout root-ca.key -sha256 -days 1024 -out root-ca.pem -subj "${subject_template}ROOTCA"
-#openssl req -x509 -new -key root-ca.key -out root-ca.pem -subj "${subject_template}ROOTCA"
+openssl x509 -noout -text -in root-ca.pem
 
-# Create a signing request with new private key for host
-#csr=$(openssl req -newkey rsa:${bits} -nodes -keyout ${host}.key -subj "${subject_template}${host}")
-#openssl req -new -config <(echo "$config") -extensions EXT -reqexts EXT -newkey rsa:${bits} -nodes -keyout ${host}.key -subj "${subject_template}${host}" -days ${days} -sha256 -out ${host}.csr
-#openssl req -new -newkey rsa:${bits} -nodes -keyout ${host}.key -subj "${subject_template}${host}" -days ${days} -sha256 -out ${host}.csr
-csr=$(openssl req -new -newkey rsa:${bits} -nodes -keyout ${host}.key -subj "${subject_template}${host}" -days ${days} -sha256)
+# Process hosts
+for host in $hosts; do
+    # Create a signing request with new private key for host
+    csr=$(openssl req -new -newkey rsa:${bits} -nodes -keyout ${host}.key -subj "${subject_template}${host}" -days ${days} -sha256)
+    #openssl req -noout -text -in <(echo "$csr")
 
-#openssl genrsa -out ${host}.key ${bits}
-#openssl req -config <(echo "$config") -newkey rsa:${bits} -nodes -keyout ${host}.key -subj "${subject_template}${host}" -days ${days} -sha256 -out ${host}.csr
-#openssl req -new -key root-ca.key -extensions EXT -config <(echo "$config") -subj "${subject_template}${host}" -days ${days} -sha256 -out ${host}.csr
-#openssl req -new -key root-ca.key -config <(echo "$config") -extensions usr_cert -subj "${subject_template}${host}" -days ${days} -sha256 -out ${host}.csr
-openssl req -noout -text -in <(echo "$csr")
+    # Create certificate signed with its private key and issued by our RootCA
+    openssl x509 -req -extfile <(echo "$ext") -extensions EXT -CA root-ca.pem -CAkey root-ca.key -CAcreateserial -days ${days} -sha256 -out ${host}.crt -in <(echo "$csr")
+    cat ${host}.crt ${host}.key > ${host}.pem
+    echo
+    echo ${host}
+    echo
+    openssl x509 -noout -text -in ${host}.pem
+done
 
-# Create certificate and sign using the private key from our RootCA
-#openssl x509 -req -CA root-ca.crt -CAkey root-ca.key -CAcreateserial -days ${days} -sha256 -out ${host}.crt -in <(echo "${csr}")
-#openssl x509 -req -extensions EXT -config <(echo "$config") -days ${days} -sha256 -out ${host}.crt -in ${host}.csr
-#openssl x509 -req -CA root-ca.pem -CAkey root-ca.key -CAcreateserial -days ${days} -sha256 -out ${host}.crt -in ${host}.csr
-#openssl ca -batch -name SigningCA -config root-ca.cfg -extensions member_ext -extfile extensions.txt -out ${host}.crt -infiles ${host}.csr
-#openssl ca -cert root-ca.pem -keyfile root-ca.key -outdir . -out ${host}.crt -infiles ${host}.csr
-#openssl x509 -req -extfile <(echo "$ext") -extensions EXT -CA root-ca.pem -CAkey root-ca.key -CAcreateserial -days ${days} -sha256 -out ${host}.crt -in ${host}.csr
-openssl x509 -req -extfile <(echo "$ext") -extensions EXT -CA root-ca.pem -CAkey root-ca.key -CAcreateserial -days ${days} -sha256 -out ${host}.crt -in <(echo "$csr")
-cat ${host}.crt ${host}.key > ${host}.pem
-openssl x509 -noout -text -in ${host}.pem
-exit 0
-
-openssl x509 -req -CA root-ca.crt -CAkey root-ca.key -CAcreateserial -extensions EXT -config <(echo "$config") -newkey rsa:${bits} -days ${days} -nodes -sha256 -out mongodb.crt -keyout mongodb.key -subj "${subject_template}${host}"
-cat *.key *.crt > mongodb.pem
-openssl x509 -noout -text -in root-ca.crt
-openssl x509 -noout -text -in mongodb.pem
+echo
+echo
 
 #openssl x509 -noout -modulus -in server.crt| openssl md5
 #openssl rsa -noout -modulus -in server.key| openssl md5
