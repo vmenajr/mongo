6440c0bf8834164f90248609b44b627bcc822bab
Vick Mena <vickmena@gmail.com>
2018-07-16 19:54:34 -0400

    Switch timeouts

--- drivers/java/new_rsnode/reconfig.sh
+++ drivers/java/new_rsnode/reconfig.sh
@@ -5,12 +5,12 @@ function print() {
 
 print Adding localhost:27020
 mongo --quiet --norc --eval 'printjson(rs.add("localhost:27020"));'
-print Sleep 2 seconds...
-sleep 2
+print Sleep 5 seconds...
+sleep 5
 print Make 27020 the new primary
 mongo --quiet --norc --eval 'cfg=rs.conf(); cfg.members[3].priority=999; printjson(rs.reconfig(cfg));'
-print Sleep for 10 seconds...
-sleep 10
+print Sleep for 15 seconds...
+sleep 15
 print Remove all nodes except for 27020
 mongo --quiet --norc --host localhost:27020 --eval 'cfg=rs.conf(); cfg.members.splice(0,3); printjson(rs.reconfig(cfg));'
 sleep 5
